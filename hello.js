7feefd41ea632c78513966873154e7756fbb3046
ypoonia <ypoonia2172gmail.com>
2024-04-18 23:59:54 +0530

    hii first commit

---
+++ hello.js
@@ -0,0 +1,12 @@
+var a = "hello"
+console.log(a);
+var aa = [12,43,123,1,2,3,4,5,5];
+aa.sort(function(a, b){return a - b}); // only way to sort a array in numeric form 
+const cc = ["ram","zen","ayush","piyush"]
+cc.sort();
+console.log(cc)
+console.log(aa)
+for(let i =0; i<aa.length;i++){
+    console.log(aa[i]+" ")
+}
+
